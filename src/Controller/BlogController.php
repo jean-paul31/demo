460efe087b13ce52803a0a0f6d92a7e782f55e1f
Hamelet <58843994+Hamelet@users.noreply.github.com>
2020-09-19 22:02:21 +0200

    first commit symfony

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repo)
+    {
+
+        $article = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('blog/home.html.twig', [
+            'title' => "bienvenue ici les amis ;",
+            'age' => 31
+        ]);
+    }
+
+       /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager){
+
+        if (!$article) {
+             $article = new Article();
+        }
+        
+
+        // $form = $this->createFormBuilder($article)
+        //              ->add('title')
+        //              ->add('content')
+        //              ->add('img') 
+        //              ->getForm();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+            
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article){
+
+        return $this->render('blog/show.html.twig', [
+            'article' =>$article
+        ]);
+    }
+
+ 
+}
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for($i = 1; $i <=3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            for ($j=1; $j <= mt_rand(4, 6) ; $j++) { 
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImg($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+                        
+                $manager->persist($article);
+
+                for ($k=0; $k <= mt_rand(4, 10) ; $k++) { 
+                    $comment = new Comment();
+
+                    $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+                    $days = (new\DateTime())->diff($article->getCreatedAt())->days;
+                    
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween('-' . $days . 'days'))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+
+                }
+            }
+
+        }
+
+        
+
+        $manager->flush();
+    }
+}
